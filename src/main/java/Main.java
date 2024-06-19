b0e9421cc1870553337eb6912494ff8b0c1de9b8
camuflaz <73291870+camuflaz@users.noreply.github.com>
2024-06-19 12:28:59 +0000

    Zaliczenie_Ocena_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    /*	10. Napisz program, który konwertuje wszystkie znaki we wprowadzonym ciągu na małe litery:
+    - Program przyjmuje łańcuch znaków wprowadzony przez użytkownika.
+    - Konwertuje wszystkie znaki w łańcuchu na małe litery.
+    - Przetworzony łańcuch jest wyświetlany na ekranie.
+      */
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj ciąg znaków: ");
+    String input = scanner.nextLine();
+    System.out.println(input.toLowerCase());
+    
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+
 }
